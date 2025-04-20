1d557434b2067faff4d80405d70da8b889a1b86b
z10mx7 <88916001+z10mx7@users.noreply.github.com>
2025-04-21 01:30:27 +0330

    add: tailwindcss

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,5 @@
 import '@mantine/core/styles.css';
+import '@/app/globals.css';
 
 import React from 'react';
 import { ColorSchemeScript, mantineHtmlProps, MantineProvider } from '@mantine/core';
--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,8 @@
 import { ColorSchemeToggle } from '../components/ColorSchemeToggle/ColorSchemeToggle';
 import { Welcome } from '../components/Welcome/Welcome';
 
+// import '@/app/globals.css';
+
 export default function HomePage() {
   return (
     <>
